3bcdebb19e7a973a173c413a2b5e1b3ae6ce42b8
Jacob Tomlinson <jacob@tomlinson.email>
2019-12-28 09:37:37 +0000

    Run black

--- opsdroid_homeassistant/__init__.py
+++ opsdroid_homeassistant/__init__.py
@@ -2,5 +2,6 @@ from .connector import HassConnector, HassEvent, HassServiceCall
 from .matcher import match_hass_state_changed
 from .skill import HassSkill
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
