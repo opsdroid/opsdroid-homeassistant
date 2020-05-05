cb9b7dd718150aa00512c7e58fc9f5b7092f2530
Jacob Tomlinson <jacob@tomlinson.email>
2020-05-05 23:11:58 +0100

    Add Home Assistant demo for testing

--- opsdroid_homeassistant/tests/test_connector.py
+++ opsdroid_homeassistant/tests/test_connector.py
@@ -1,11 +1,12 @@
 import pytest
+import requests
 
 from opsdroid_homeassistant import HassConnector
 
 
 @pytest.fixture
 def dummy_config():
-    return {"token": "abc123", "url": "https://example.com:8123"}
+    return {"token": "abc123", "url": "http://127.0.0.1:8123"}
 
 
 def test_attributes(dummy_config):
@@ -13,3 +14,10 @@ def test_attributes(dummy_config):
 
     assert connector.name == "homeassistant"
     assert connector.default_target is None
+
+
+def test_hass(homeassistant, access_token):
+    response = requests.get(
+        homeassistant, headers={"Authorization": "Bearer " + access_token}
+    )
+    assert response.status_code == 200
