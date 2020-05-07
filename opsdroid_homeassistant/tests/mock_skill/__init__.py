42bfcc28d71fbf4033c4685130fb24efcdf99140
Jacob Tomlinson <jacobtomlinson@users.noreply.github.com>
2020-05-07 21:56:07 +0100

    Add test for match_hass_state_changed and fix bug at the same time 💪 (#15)

--- opsdroid_homeassistant/tests/mock_skill/__init__.py
+++ opsdroid_homeassistant/tests/mock_skill/__init__.py
@@ -1,11 +1,12 @@
 from opsdroid.matchers import match_regex
-from opsdroid_homeassistant import HassSkill
+from opsdroid_homeassistant import HassSkill, match_hass_state_changed
 
 
 class TestSkill(HassSkill):
     def __init__(self, opsdroid, config, *args, **kwargs):
         super().__init__(opsdroid, config, *args, **kwargs)
         opsdroid.mock_skill = self
+        self.kitchen_lights_changed = False
 
     @match_regex(r"Turn on the light")
     async def lights_on(self, event):
@@ -18,3 +19,7 @@ class TestSkill(HassSkill):
     @match_regex(r"Toggle the light")
     async def lights_toggle(self, event):
         await self.toggle("light.bed_light")
+
+    @match_hass_state_changed("light.kitchen_lights")
+    async def listen_for_lights(self, event):
+        self.kitchen_lights_changed = True
